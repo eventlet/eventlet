cd1b86488ea27bed98deb9832ca53f54b986eb42
donovan <donovan@bigass.local>
2008-07-07 13:17:39 -0700

    Add a NEWS file and an example of using the wsgi server along with a link to Spawning; Set version number to 0.6.1 for a release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='eventlet',
-    version='0.7pre',
+    version='0.6.1',
     description='Coroutine-based networking library',
     author='Linden Lab',
     author_email='eventletdev@lists.secondlife.com',
