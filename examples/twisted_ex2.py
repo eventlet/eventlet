058d21e672d053c058377d61e54c367a47ab8d23
Denis Bilenko <denis@ag-projects.com>
2008-10-10 14:57:25 +0700

    added an example - wrapping twisted protocol using a channel and block_on()

---
+++ examples/twisted_ex2.py
@@ -0,0 +1,56 @@
+"wrapping twisted protocol using a channel and block_on()"
+from twisted.internet import pollreactor; pollreactor.install()
+from twisted.internet.protocol import ClientCreator
+from twisted.protocols import basic
+from twisted.internet import reactor
+from twisted.internet.error import ConnectionDone
+
+from eventlet.channel import channel
+from eventlet.twisteds.util import block_on
+
+
+class LineOnlyReceiver(basic.LineOnlyReceiver):
+
+    def lineReceived(self, line):
+        self.channel.send(line)
+
+    def sendLine(self, line):
+        return self.transport.writeSequence((line,self.delimiter))
+
+    def connectionLost(self, reason):
+        self.channel.send_exception(reason.value)
+
+
+class line_only_receiver:
+
+    def __init__(self, host, port):
+        cc = ClientCreator(reactor, LineOnlyReceiver)
+        self.channel = channel()
+        self.protocol = block_on(cc.connectTCP(host, port))
+        self.protocol.channel = self.channel
+
+    def readline(self):
+        return self.channel.receive()
+
+    def send(self, data):
+        self.protocol.transport.write(data)
+
+    def sendline(self, line):
+        self.protocol.sendLine(line)
+
+    # iterator protocol:
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        try:
+            return self.readline()
+        except ConnectionDone:
+            raise StopIteration
+
+
+conn = line_only_receiver('www.google.com', 80)
+conn.send('GET / HTTP/1.0\r\n\r\n')
+for line in conn:
+    print line
