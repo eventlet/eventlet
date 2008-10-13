3382938649045c938f10c20c6d7bba37c31f5c1e
donovan <donovan@ell-ee-dee.local>
2008-10-13 10:38:50 -0700

    0.8 is out, on to 0.9pre

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='eventlet',
-    version='0.8',
+    version='0.9pre',
     description='Coroutine-based networking library',
     author='Linden Lab',
     author_email='eventletdev@lists.secondlife.com',
