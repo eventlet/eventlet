f5dec4c58b0cab2643a19f89bd1f5964c14f6549
Denis Bilenko <denis@ag-projects.com>
2008-10-30 02:56:30 +0600

    better names for twisted examples; removed outdated example; changed xcap proxy to use join_reactor and the latest version of xcaplib

--- examples/twisted_xcap_proxy.py
+++ examples/twisted_xcap_proxy.py
@@ -1,13 +1,11 @@
-from twisted.internet import pollreactor; pollreactor.install()
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor
 from twisted.protocols import basic
 
-from xcaplib.client import XCAPClient
+from xcaplib.green import XCAPClient
 
-from eventlet.api import get_hub
 from eventlet.twisteds.util import callInGreenThread
-
+from eventlet.twisteds import join_reactor
 
 class LineOnlyReceiver(basic.LineOnlyReceiver):
 
@@ -28,15 +26,6 @@ class LineOnlyReceiver(basic.LineOnlyReceiver):
 class MyFactory(Factory):
     protocol = LineOnlyReceiver
 
-def xcaplib_enable_eventlet():
-    from eventlet.green import urllib2
-    from xcaplib import httpclient
-    # replacing all the references to the old urllib2 in xcaplib:
-    httpclient.urllib2 = urllib2
-    httpclient.HTTPRequest.__bases__ = (urllib2.Request,)
-
-xcaplib_enable_eventlet()
 client = XCAPClient('https://xcap.sipthor.net/xcap-root', 'alice@example.com', '123')
 reactor.listenTCP(8007, MyFactory())
-get_hub().switch()
-
+reactor.run()
