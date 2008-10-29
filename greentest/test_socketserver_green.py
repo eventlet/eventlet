fa461698088b180adf5636fa1365583a6c0523f5
Denis Bilenko <denis@ag-projects.com>
2008-10-30 03:06:22 +0600

    removed pollreactor.install() where it does not make sense anymore

--- greentest/test_socketserver_green.py
+++ greentest/test_socketserver_green.py
@@ -1,6 +1,4 @@
 import sys
-if 'twisted.internet.reactor' not in sys.modules:
-    from twisted.internet import pollreactor; pollreactor.install()
 from eventlet.green.SocketServer import *
 from eventlet.green import socket
 from eventlet.green import select
--- greentest/test_urllib2_localnet_green.py
+++ greentest/test_urllib2_localnet_green.py
@@ -1,9 +1,4 @@
 import sys
-if 'twisted.internet.reactor' not in sys.modules:
-    # the following line makes a difference on my machine, tests take
-    # muuuch less time (5x) to complete compared to selectreactor
-    from twisted.internet import pollreactor; pollreactor.install()
-
 from eventlet.green import threading
 from eventlet.green import socket
 from eventlet.green import urllib2
