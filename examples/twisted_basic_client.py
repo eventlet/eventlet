3838c8f032b43ac9bb2cdba4eb058a4e4a4d997a
Denis Bilenko <denis@ag-projects.com>
2008-11-05 17:10:28 +0600

    added example for ssl and LineOnlyReceiver to twisted_basic_client.py

--- examples/twisted_basic_client.py
+++ examples/twisted_basic_client.py
@@ -1,3 +1,5 @@
+from twisted.internet import ssl
+from twisted.internet.error import ConnectionClosed
 from eventlet.twistedutil.protocol import BufferCreator
 from eventlet.twistedutil.protocols.basic import LineOnlyReceiverBuffer
 
@@ -7,4 +9,13 @@ from twisted.internet import reactor
 conn = BufferCreator(reactor).connectTCP('www.google.com', 80)
 conn.write('GET / HTTP/1.0\r\n\r\n')
 print conn.read()
+
+# read from SSL connection line by line
+conn = BufferCreator(reactor, LineOnlyReceiverBuffer).connectSSL('sf.net', 443, ssl.ClientContextFactory())
+conn.write('GET / HTTP/1.0\r\n\r\n')
+try:
+    for num, line in enumerate(conn):
+        print '%3s %r' % (num, line)
+except ConnectionClosed, ex:
+    print ex
 
