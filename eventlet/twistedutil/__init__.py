fed965ff3cbd40fa254137742d15ba3fd102479a
Denis Bilenko <denis@ag-projects.com>
2008-11-17 13:13:54 +0600

    added simple test for block_on

--- eventlet/twistedutil/__init__.py
+++ eventlet/twistedutil/__init__.py
@@ -38,3 +38,27 @@ def deferToGreenThread(func, *args, **kwargs):
 def callInGreenThread(func, *args, **kwargs):
     return spawn(func, *args, **kwargs)
 
+
+if __name__=='__main__':
+    import sys
+    num = int(sys.argv[1])
+    from twisted.internet import reactor
+    def test():
+        print block_on(reactor.resolver.getHostByName('www.google.com'))
+        print block_on(reactor.resolver.getHostByName('###'))
+    if num==0:
+        test()
+    elif num==1:
+        spawn(test)
+        from eventlet.api import sleep
+        sleep(5)
+    elif num==2:
+        from eventlet.twistedutil import join_reactor
+        spawn(test)
+        reactor.run()
+    elif num==3:
+        from eventlet.twistedutil import join_reactor
+        print "fails because it's impossible to use block_on from the mainloop"
+        reactor.callLater(0, test)
+        reactor.run()
+
