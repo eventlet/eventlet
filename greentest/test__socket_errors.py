db6262a6c5917b5bf7e8db17b21109396e7d3a42
Denis Bilenko <denis@ag-projects.com>
2009-01-12 15:32:26 +0600

    added test__socket_errors.py

---
+++ greentest/test__socket_errors.py
@@ -0,0 +1,21 @@
+import unittest
+from eventlet import api
+
+if hasattr(api._threadlocal, 'hub'):
+    from eventlet.green import socket
+else:
+    import socket
+
+class TestSocketErrors(unittest.TestCase):
+    
+    def test_connection_refused(self):
+        s = socket.socket()
+        try:
+            s.connect(('127.0.0.1', 81))
+        except socket.error, ex:
+            code, text = ex.args
+            assert code == 111, (code, text)
+            assert 'refused' in text.lower(), (code, text)
+
+if __name__=='__main__':
+    unittest.main()
