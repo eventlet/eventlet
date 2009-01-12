e2bb90729daed14140be4801ae2c41adfcb37c98
Denis Bilenko <denis@ag-projects.com>
2009-01-13 00:18:31 +0600

    updated copyrights

--- greentest/runall.py
+++ greentest/runall.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-# Copyright (c) 2008 Denis Bilenko
+# Copyright (c) 2008-2009 AG Projects
+# Author: Denis Bilenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
