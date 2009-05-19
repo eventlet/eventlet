ac7ce888c0739c277ccb047154575e687c7a69e9
Denis Bilenko <denis.bilenko@gmail.com>
2009-05-19 23:36:10 +0700

    runall.py: do not exclude test_threading.py, it works OK now

--- greentest/runall.py
+++ greentest/runall.py
@@ -39,7 +39,7 @@ PARSE_PERIOD = 10
 # the following aren't in the default list unless --all option present
 NOT_HUBS = set(['nginx'])
 NOT_REACTORS = set(['wxreactor', 'glib2reactor', 'gtk2reactor'])
-NOT_TESTS = set(['test_threading.py'])
+NOT_TESTS = set()
 
 def w(s):
     sys.stderr.write("%s\n" % (s, ))
