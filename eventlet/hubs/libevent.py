a420bcb6d82aff352301b59c8bb3bbd27d6da1ab
Denis Bilenko <denis.bilenko@gmail.com>
2009-06-24 18:50:35 +0700

    libevent: add _version_info() method
    it provides info about python-event's and libevent's versions and the polling method

--- eventlet/hubs/libevent.py
+++ eventlet/hubs/libevent.py
@@ -196,6 +196,19 @@ class Hub(object):
     def get_excs(self):
         return self.excs
 
+    def _version_info(self):
+        baseversion = event.__version__
+        try:
+            from ctypes import CDLL, util, c_char_p
+            c = CDLL(util.find_library('event'))
+            c.event_get_version.restype = c_char_p
+            baseversion += '/libevent=%s' % (c.event_get_version(), )
+            c.event_get_method.restype = c_char_p
+            baseversion += '/method=%s' % (c.event_get_method(), )
+        except Exception, ex:
+            print ex or type(ex).__name__
+        return baseversion
+
 
 def _scheduled_call(event_impl, handle, evtype, arg):
     cb, args, kwargs = arg
