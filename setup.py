2e33af03b733969b4de6fd0d4b6864e9d37dce23
Ryan Williams <rdw@lindenlab.com>
2009-08-18 00:04:07 -0700

    Added command line flags for disabling dependencies on pyopenssl or greenlet.

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@
 
 from setuptools import find_packages, setup
 from eventlet import __version__
+import sys
 
+requirements = []
+for flag, req in [('--without-greenlet','greenlet >= 0.2'),
+                  ('--without-pyopenssl', 'pyopenssl')]:
+    if flag in sys.argv:
+        sys.argv.remove(flag)
+    else:
+        requirements.append(req)
 
 setup(
     name='eventlet',
@@ -13,7 +21,7 @@ setup(
     author_email='eventletdev@lists.secondlife.com',
     url='http://eventlet.net',
     packages=find_packages(exclude=['tests']),
-    install_requires=['greenlet >= 0.2', 'pyopenssl'],
+    install_requires=requirements,
     long_description="""
     Eventlet is a networking library written in Python. It achieves
     high scalability by using non-blocking io while at the same time
