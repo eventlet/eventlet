773633b6873f2b0f8b7e14e8150e5a34f7058af6
Ryan Williams <rdw@lindenlab.com>
2009-11-27 12:19:12 -0500

    Patcher-ized test_socket_ssl and test_socketserver.  Also patcher-ized SocketServer, which allows it to successfullly pass its unit tests, which it failed previously because it was making a call to non-greened select.select().

--- eventlet/green/SocketServer.py
+++ eventlet/green/SocketServer.py
@@ -1,59 +1,12 @@
-__import_lst = ['__all__', '__version__', 'BaseServer', 'TCPServer', 'UDPServer', 'ForkingMixIn',
-                'ThreadingMixIn', 'BaseRequestHandler', 'StreamRequestHandler', 'DatagramRequestHandler']
-__SocketServer = __import__('SocketServer')
-for var in __import_lst:
-    exec "%s = __SocketServer.%s" % (var, var)
-
-
-# QQQ ForkingMixIn should be fixed to use green waitpid?
+from eventlet import patcher
 
 from eventlet.green import socket
+from eventlet.green import select
+from eventlet.green import threading
+patcher.inject('SocketServer',
+    globals(),
+    ('socket', socket),
+    ('select', select),
+    ('threading', threading))
 
-class TCPServer(TCPServer):
-
-    def __init__(self, server_address, RequestHandlerClass):
-        """Constructor.  May be extended, do not override."""
-        BaseServer.__init__(self, server_address, RequestHandlerClass)
-        self.socket = socket.socket(self.address_family,
-                                    self.socket_type)
-        self.server_bind()
-        self.server_activate()
-
-class UDPServer(UDPServer):
-
-    def __init__(self, server_address, RequestHandlerClass):
-        """Constructor.  May be extended, do not override."""
-        BaseServer.__init__(self, server_address, RequestHandlerClass)
-        self.socket = socket.socket(self.address_family,
-                                    self.socket_type)
-        self.server_bind()
-        self.server_activate()
-
-class ThreadingMixIn(ThreadingMixIn):
-
-    def process_request(self, request, client_address):
-        """Start a new thread to process the request."""
-        from eventlet.green import threading
-        t = threading.Thread(target = self.process_request_thread,
-                             args = (request, client_address))
-        if self.daemon_threads:
-            t.setDaemon (1)
-        t.start()
-
-class ForkingUDPServer(ForkingMixIn, UDPServer): pass
-class ForkingTCPServer(ForkingMixIn, TCPServer): pass
-
-class ThreadingUDPServer(ThreadingMixIn, UDPServer): pass
-class ThreadingTCPServer(ThreadingMixIn, TCPServer): pass
-
-if hasattr(socket, 'AF_UNIX'):
-
-    class UnixStreamServer(TCPServer):
-        address_family = socket.AF_UNIX
-
-    class UnixDatagramServer(UDPServer):
-        address_family = socket.AF_UNIX
-
-    class ThreadingUnixStreamServer(ThreadingMixIn, UnixStreamServer): pass
-    class ThreadingUnixDatagramServer(ThreadingMixIn, UnixDatagramServer): pass
-
+# QQQ ForkingMixIn should be fixed to use green waitpid?
