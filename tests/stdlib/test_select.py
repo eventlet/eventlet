e36cdaaa7fcd277cec97273533a2b42835e243e4
Ryan Williams <rdw@lindenlab.com>
2009-11-26 23:32:43 -0500

    Convert test_select to patcher

--- tests/stdlib/test_select.py
+++ tests/stdlib/test_select.py
@@ -1,11 +1,12 @@
 from eventlet import api
 api.sleep(0)  # initialize the hub
+from eventlet import patcher
 from eventlet.green import select
-import sys
-sys.modules['select'] = select
 
-from test.test_select import *
-
+patcher.inject('test.test_select',
+    globals(),
+    ('select', select))
+    
 if __name__ == "__main__":
     try:
         test_main()
