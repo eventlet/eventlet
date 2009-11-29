7958daa396e614f1ffdce3db5e9c5aecc9afe59a
Ryan Williams <rdw@lindenlab.com>
2009-11-29 03:15:15 -0500

    Greened asynchat

---
+++ tests/stdlib/test_asynchat.py
@@ -0,0 +1,19 @@
+from eventlet import patcher
+from eventlet.green import asyncore
+from eventlet.green import asynchat
+from eventlet.green import socket
+from eventlet.green import thread
+from eventlet.green import threading
+from eventlet.green import time
+
+patcher.inject("test.test_asynchat",
+    globals(),
+    ('asyncore', asyncore),
+    ('asynchat', asynchat),
+    ('socket', socket),
+    ('thread', thread),
+    ('threading', threading),
+    ('time', time))
+    
+if __name__ == "__main__":
+    test_main()
