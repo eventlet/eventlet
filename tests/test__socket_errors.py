a9d834b2dd08c83fc61ce53b8fdd22f1b10f8f42
Ryan Williams <rdw@lindenlab.com>
2009-12-11 11:46:50 -0800

    Something started listening on port 81, and this test started failing.  Changed so that it selects a port in a more foolproof manner.

--- tests/test__socket_errors.py
+++ tests/test__socket_errors.py
@@ -9,9 +9,16 @@ else:
 class TestSocketErrors(unittest.TestCase):
     
     def test_connection_refused(self):
+        # open and close a dummy server to find an unused port
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 0))
+        server.listen(1)
+        port = server.getsockname()[1]
+        server.close()
+        del server
         s = socket.socket()
         try:
-            s.connect(('127.0.0.1', 81))
+            s.connect(('127.0.0.1', port))
             self.fail("Shouldn't have connected")
         except socket.error, ex:
             code, text = ex.args
