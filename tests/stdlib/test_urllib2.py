ee9023c3a9f21d5a6c52dadc50a7400384302590
Ryan Williams <rdw@lindenlab.com>
2010-01-28 13:11:57 -0800

    test_urllib2 is a bit funky

--- tests/stdlib/test_urllib2.py
+++ tests/stdlib/test_urllib2.py
@@ -8,6 +8,7 @@ patcher.inject('test.test_urllib2',
     ('urllib2', urllib2))
 
 HandlerTests.test_file = patcher.patch_function(HandlerTests.test_file, ('socket', socket))
+HandlerTests.test_cookie_redirect = patcher.patch_function(HandlerTests.test_cookie_redirect, ('urllib2', urllib2))
 try:
     OpenerDirectorTests.test_badly_named_methods = patcher.patch_function(OpenerDirectorTests.test_badly_named_methods, ('urllib2', urllib2))
 except AttributeError:
