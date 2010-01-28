06801e2948583c32f843924e29b48233e8bbf63a
Ryan Williams <rdw@lindenlab.com>
2010-01-27 23:59:32 -0800

    Wrapped imports in try/except so as to make it easier to find out the version number when dependencies are missing, for gholt's use case.

--- eventlet/__init__.py
+++ eventlet/__init__.py
@@ -1,20 +1,25 @@
 version_info = (0, 9, 3)
 __version__ = '%s.%s.%s' % version_info
 
-from eventlet import greenthread
-from eventlet import greenpool
-from eventlet import queue
+try:
+    from eventlet import greenthread
+    from eventlet import greenpool
+    from eventlet import queue
 
-sleep = greenthread.sleep
-
-spawn = greenthread.spawn
-spawn_n = greenthread.spawn_n
-call_after_global = greenthread.call_after_global
-TimeoutError = greenthread.TimeoutError
-exc_after = greenthread.exc_after
-with_timeout = greenthread.with_timeout
-
-GreenPool = greenpool.GreenPool
-GreenPile = greenpool.GreenPile
-
-Queue = queue.Queue
+    sleep = greenthread.sleep
+    
+    spawn = greenthread.spawn
+    spawn_n = greenthread.spawn_n
+    call_after_global = greenthread.call_after_global
+    TimeoutError = greenthread.TimeoutError
+    exc_after = greenthread.exc_after
+    with_timeout = greenthread.with_timeout
+    
+    GreenPool = greenpool.GreenPool
+    GreenPile = greenpool.GreenPile
+    
+    Queue = queue.Queue
+except ImportError:
+    # this is to make Debian packaging easier
+    import traceback
+    traceback.print_exc()
