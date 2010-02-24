da4448f81139be41f7b10b82d4e9f58f227f065b
Ryan Williams <rdw@lindenlab.com>
2010-02-24 04:33:51 -0500

    Reimplemented corolocal so as to implement __init__, local function calling, and, amazing, also leak fixing.  Fixes #41.

--- tests/thread_test.py
+++ tests/thread_test.py
@@ -1,9 +1,11 @@
+import weakref
 from eventlet.green import thread
 from eventlet import greenthread
 from eventlet import event
 import eventlet
+from eventlet import corolocal
 
-from tests import LimitedTestCase
+from tests import LimitedTestCase, skipped
 
 class Locals(LimitedTestCase):
     def passthru(self, *args, **kw):
@@ -18,6 +20,7 @@ class Locals(LimitedTestCase):
         self.results = []
         super(Locals, self).tearDown()
 
+    @skipped  # cause it relies on internal details of corolocal that are no longer true
     def test_simple(self):
         tls = thread._local()
         g_ids = []
@@ -37,3 +40,72 @@ class Locals(LimitedTestCase):
         self.failUnlessRaises(AttributeError, lambda: tls.value)
         evt.send("done")
         eventlet.sleep()
+
+    def test_assignment(self):
+        my_local = corolocal.local()
+        my_local.a = 1
+        def do_something():
+            my_local.b = 2
+            self.assertEqual(my_local.b, 2)
+            try:
+                my_local.a
+                self.fail()
+            except AttributeError:
+                pass
+        eventlet.spawn(do_something).wait()
+        self.assertEqual(my_local.a, 1)
+        
+    def test_calls_init(self):
+        init_args = []
+        class Init(corolocal.local):
+            def __init__(self, *args):
+                init_args.append((args, eventlet.getcurrent()))
+        
+        my_local = Init(1,2,3)
+        self.assertEqual(init_args[0][0], (1,2,3))
+        self.assertEqual(init_args[0][1], eventlet.getcurrent())
+        
+        def do_something():
+            my_local.foo = 'bar'
+            self.assertEqual(len(init_args), 2, init_args)
+            self.assertEqual(init_args[1][0], (1,2,3))
+            self.assertEqual(init_args[1][1], eventlet.getcurrent())
+            
+        eventlet.spawn(do_something).wait()
+        
+    def test_calling_methods(self):
+        class Caller(corolocal.local):
+            def callme(self):
+                return self.foo
+        
+        my_local = Caller()
+        my_local.foo = "foo1"
+        self.assertEquals("foo1", my_local.callme())
+        
+        def do_something():
+            my_local.foo = "foo2"
+            self.assertEquals("foo2", my_local.callme())
+            
+        eventlet.spawn(do_something).wait()        
+            
+        my_local.foo = "foo3"
+        self.assertEquals("foo3", my_local.callme())
+        
+    def test_no_leaking(self):
+        refs = weakref.WeakKeyDictionary()
+        my_local = corolocal.local()
+        class X(object):
+            pass
+        def do_something(i):
+            o = X()
+            refs[o] = True
+            my_local.foo = o
+            
+        p = eventlet.GreenPool()
+        for i in xrange(100):
+            p.spawn(do_something, i)
+        p.waitall()
+        del p
+        # at this point all our coros have terminated
+        self.assertEqual(len(refs), 1)
+        
