4ec2cc509f249a66717d885bc59e4e4a9a105590
Ryan Williams <rdw@lindenlab.com>
2010-02-24 22:28:57 -0800

    Fixed bad references in test_socket_ssl.py

--- tests/stdlib/test_socket_ssl.py
+++ tests/stdlib/test_socket_ssl.py
@@ -5,6 +5,16 @@ from eventlet.green import socket
 from eventlet.green import urllib
 from eventlet.green import threading
 
+# enable network resource
+import test.test_support
+i_r_e = test.test_support.is_resource_enabled
+def is_resource_enabled(resource):
+    if resource == 'network':
+        return True
+    else:
+        return i_r_e(resource)
+test.test_support.is_resource_enabled = is_resource_enabled
+
 try:
     socket.ssl
     socket.sslerror
@@ -21,6 +31,13 @@ test_basic = patcher.patch_function(test_basic,
     ('urllib', urllib))    
 test_rude_shutdown = patcher.patch_function(test_rude_shutdown, 
     ('threading', threading))
+
+def test_main():
+    if not hasattr(socket, "ssl"):
+        raise test_support.TestSkipped("socket module has no ssl support")
+    test_rude_shutdown()
+    test_basic()
+    test_timeout()
 
 
 if __name__ == "__main__":
