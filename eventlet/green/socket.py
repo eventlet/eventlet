cac3d24f2fdc47d96829a4d9a86b999e2cc968b3
Ryan Williams <rdw@lindenlab.com>
2010-02-27 17:59:50 -0800

    Conditional import of attributes.

--- eventlet/green/socket.py
+++ eventlet/green/socket.py
@@ -14,14 +14,20 @@ import warnings
 __patched__ = ['fromfd', 'socketpair', 'gethostbyname', 'create_connection',
                'ssl', 'socket']
 
-__original_fromfd__ = __socket.fromfd
-def fromfd(*args):
-    return socket(__original_fromfd__(*args))
+try:
+    __original_fromfd__ = __socket.fromfd
+    def fromfd(*args):
+        return socket(__original_fromfd__(*args))
+except AttributeError:
+    pass
 
-__original_socketpair__ = __socket.socketpair
-def socketpair(*args):
-    one, two = __original_socketpair__(*args)
-    return socket(one), socket(two)
+try:
+    __original_socketpair__ = __socket.socketpair
+    def socketpair(*args):
+        one, two = __original_socketpair__(*args)
+        return socket(one), socket(two)
+except AttributeError:
+    pass
 
 __original_gethostbyname__ = __socket.gethostbyname
 def gethostbyname(name):
