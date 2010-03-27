f2cd9156e8ad95b1b2136f92e69d69dcf73768a9
Ryan Williams <breath@alum.mit.edu>
2010-03-27 15:11:00 -0700

    Add Lock to list of patched threading primitives.  Fixes mcarter's sqlalchemy bug.

--- eventlet/green/threading.py
+++ eventlet/green/threading.py
@@ -3,7 +3,7 @@ from eventlet.green import thread
 from eventlet.green import time
 
 __patched__ = ['_start_new_thread', '_allocate_lock', '_get_ident', '_sleep',
-               'local', 'stack_size']
+               'local', 'stack_size', 'Lock']
 
 patcher.inject('threading',
     globals(),
