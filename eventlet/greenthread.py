b35c488ff7b6fb9e59d5f76b40a8c570dbfbb81b
Ryan Williams <breath@alum.mit.edu>
2010-03-29 00:08:03 -0700

    Clarified docs on spawn_n's semantics.

--- eventlet/greenthread.py
+++ eventlet/greenthread.py
@@ -54,9 +54,15 @@ def _main_wrapper(func, args, kwargs):
 
 
 def spawn_n(func, *args, **kwargs):
-    """Same as :func:`spawn`, but returns a ``greenlet`` object from which it is 
-    not possible to retrieve the results.  This is faster than :func:`spawn`;
-    it is fastest if there are no keyword arguments."""
+    """Same as :func:`spawn`, but returns a ``greenlet`` object from
+    which it is not possible to retrieve either a return value or
+    whether it raised any exceptions.  This is faster than
+    :func:`spawn`; it is fastest if there are no keyword arguments.
+    
+    If an exception is raised in the function, spawn_n prints a stack
+    trace; the print can be disabled by calling
+    :func:`eventlet.debug.hub_exceptions` with False.
+    """
     return _spawn_n(0, func, args, kwargs)[1]
     
     
