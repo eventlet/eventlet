d47ee516fd3acd1293cfb8981618dde2c2e77f38
Ryan Williams <breath@alum.mit.edu>
2010-08-24 17:42:16 -0700

    Removed EVENTLET_TPOOL_DNS because greendns is better.  Fixed issue where we were instantiating the hub too early in one use case, added a test for it, thanks to pigmej and gholt.

--- eventlet/green/socket.py
+++ eventlet/green/socket.py
@@ -18,46 +18,9 @@ if os.environ.get("EVENTLET_NO_GREENDNS",'').lower() != "yes":
     except ImportError:
         pass
 
-__original_gethostbyname__ = __socket.gethostbyname
-# the thread primitives on Darwin have some bugs that make
-# it undesirable to use tpool for hostname lookups
-_can_use_tpool = (
-    os.environ.get("EVENTLET_TPOOL_DNS",'').lower() == "yes"
-    and not sys.platform.startswith('darwin'))
-def _gethostbyname_twisted(name):
-    from twisted.internet import reactor
-    from eventlet.twistedutil import block_on as _block_on
-    return _block_on(reactor.resolve(name))
-
-def _gethostbyname_tpool(name):
-    from eventlet import tpool
-    return tpool.execute(
-        __original_gethostbyname__, name)
-
-if getattr(get_hub(), 'uses_twisted_reactor', None):
-    gethostbyname = _gethostbyname_twisted
-elif greendns:
+if greendns:
     gethostbyname = greendns.gethostbyname
-elif _can_use_tpool:
-    gethostbyname = _gethostbyname_tpool
-else:
-    gethostbyname = __original_gethostbyname__
-
-
-__original_getaddrinfo__ = __socket.getaddrinfo
-def _getaddrinfo_tpool(*args, **kw):
-    from eventlet import tpool
-    return tpool.execute(
-        __original_getaddrinfo__, *args, **kw)
-
-if greendns:
     getaddrinfo = greendns.getaddrinfo
-elif _can_use_tpool:
-    getaddrinfo = _getaddrinfo_tpool
-else:
-    getaddrinfo = __original_getaddrinfo__
-
-if greendns:
     gethostbyname_ex = greendns.gethostbyname_ex
     getnameinfo = greendns.getnameinfo
     __patched__ + ['gethostbyname_ex', 'getnameinfo']
