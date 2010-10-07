f26ccf74517c1e63dd2c9c6885b4214a52447325
Ryan Williams <breath@alum.mit.edu>
2010-10-07 11:22:57 -0700

    Finally, eventlet.green.MySQLdb so we don't have to hassle with db_pool if we're not going to be using the pooling functionality.

---
+++ eventlet/green/MySQLdb.py
@@ -0,0 +1,32 @@
+__MySQLdb = __import__('MySQLdb')
+globals().update(dict([(var, getattr(__MySQLdb, var))
+                       for var in dir(__MySQLdb)
+                       if not var.startswith('__')]))
+                       
+__all__ = __MySQLdb.__all__
+__patched__ = ["connect", "Connect", 'Connection', 'connections']
+
+from eventlet import tpool
+
+__orig_connections = __import__('MySQLdb.connections').connections
+
+def Connection(*args, **kw):
+    conn = tpool.execute(__orig_connections.Connection, *args, **kw)
+    return tpool.Proxy(conn, autowrap_names=('cursor',))
+connect = Connect = Connection
+
+# replicate the MySQLdb.connections module but with a tpooled Connection factory
+class MySQLdbConnectionsModule(object):
+    pass
+connections = MySQLdbConnectionsModule()
+for var in dir(__orig_connections):
+    if not var.startswith('__'):
+        setattr(connections, var, getattr(__orig_connections, var))
+connections.Connection = Connection
+
+cursors = __import__('MySQLdb.cursors').cursors
+converters = __import__('MySQLdb.converters').converters
+
+# TODO support instantiating cursors.FooCursor objects directly
+# TODO though this is a low priority, it would be nice if we supported
+# subclassing eventlet.green.MySQLdb.connections.Connection
