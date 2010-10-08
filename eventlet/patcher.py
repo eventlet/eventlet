d1ef6f39e7a14d347e7caec498b2e6b178994b11
Ryan Williams <breath@alum.mit.edu>
2010-10-08 09:21:00 -0700

    Not enabling MySQLdb patching in import_patched for now.

--- eventlet/patcher.py
+++ eventlet/patcher.py
@@ -64,8 +64,8 @@ def inject(module_name, new_globals, *additional_modules):
             _green_select_modules() +
             _green_socket_modules() +
             _green_thread_modules() +
-            _green_time_modules() + 
-            _green_MySQLdb())
+            _green_time_modules()) 
+            #_green_MySQLdb()) # enable this after a short baking-in period
     
     # after this we are gonna screw with sys.modules, so capture the
     # state of all the modules we're going to mess with, and lock
