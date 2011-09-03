e4d0e91f793be921060317a3c2601aac99e6f45c
Geoff Salmon <geoff.salmon@gmail.com>
2011-09-03 09:07:30 -0400

    send/send_multipart from multiple greenthreads working, maybe...

--- eventlet/green/zmq.py
+++ eventlet/green/zmq.py
@@ -49,6 +49,15 @@ class _Context(__zmq__.Context):
 _multi_reader_types = set([__zmq__.XREP, __zmq__.XREQ, __zmq__.SUB, __zmq__.PULL, __zmq__.PAIR])
 _multi_writer_types = set([__zmq__.XREP, __zmq__.XREQ, __zmq__.PUB, __zmq__.PUSH, __zmq__.PAIR])
 
+_disable_send_types = set([__zmq__.SUB, __zmq__.PULL])
+_disable_recv_types = set([__zmq__.PUB, __zmq__.PUSH])
+
+
+# TODO: 
+# - Ensure that recv* and send* methods raise error when called on a
+#   closed socket. They should not block.
+# - Return correct message tracker from send* methods
+
 class Socket(__zmq__.Socket):
     """Green version of :class:`zmq.core.socket.Socket
 
@@ -67,20 +76,25 @@ class Socket(__zmq__.Socket):
 
     """
 
-    def __init__(self, *args, **kwargs):
-        super(Socket, self).__init__(*args, **kwargs)
+    def __init__(self, context, socket_type):
+        super(Socket, self).__init__(context, socket_type)
 
-        if False and self.socket_type in _multi_writer_types:
+        # customize send and recv functions based on socket type
+        if socket_type in _multi_writer_types:
             # support multiple greenthreads writing at the same time
             self._writers = deque()
             self.send = self._xsafe_send
             self.send_multipart = self._xsafe_send_multipart
+        elif socket_type in _disable_send_types:
+            self.send = self.send_multipart = _send_not_supported
 
-        if False and self.socket_type in _multi_reader_types:
+        if socket_type in _multi_reader_types:
             # support multiple greenthreads reading at the same time
             self._readers = deque()
             self.recv = self._xsafe_recv
             self.recv_multipart = self._xsafe_recv_multipart
+        elif socket_type in _disable_recv_types:
+            self.recv = self.recv_multipart = _recv_not_supported
 
     def _sock_wait(self, read=False, write=False):
         """
@@ -107,7 +121,7 @@ class Socket(__zmq__.Socket):
         called in real code.
         """
         if flags & __zmq__.NOBLOCK:
-            super(Socket, self).send(msg, flags=flags, track=track, copy=copy)
+            super(Socket, self).send(msg, flags=flags, copy=copy, track=track)
             return
 
         flags |= __zmq__.NOBLOCK
@@ -115,9 +129,7 @@ class Socket(__zmq__.Socket):
         while True:
             try:
                 self._sock_wait(write=True)
-                super(Socket, self).send(msg, flags=flags, track=track,
-                                         copy=copy)
-                return
+                return super(Socket, self).send(msg, flags=flags, copy=copy, track=track)
             except __zmq__.ZMQError, e:
                 if e.errno != EAGAIN:
                     raise
@@ -129,19 +141,43 @@ class Socket(__zmq__.Socket):
         called in real code.
         """
         if flags & __zmq__.NOBLOCK:
-            return super(Socket, self).recv(flags=flags, track=track, copy=copy)
+            return super(Socket, self).recv(flags=flags, copy=copy, track=track)
 
         flags |= __zmq__.NOBLOCK
 
         while True:
             try:
                 self._sock_wait(read=True)
-                m = super(Socket, self).recv(flags=flags, track=track, copy=copy)
-                if m is not None:
-                    return m
+                return super(Socket, self).recv(flags=flags, copy=copy, track=track)
             except __zmq__.ZMQError, e:
                 if e.errno != EAGAIN:
                     raise
+
+    def _send_not_supported(self, msg, flags=0, copy=True, track=False):
+        raise __zmq__.ZMQError(__zmq__.ENOTSUP)
+
+    def _recv_not_supported(self, flags=0, copy=True, track=False):
+        raise __zmq__.ZMQError(__zmq__.ENOTSUP)
+
+
+    def _xsafe_sock_wait(self, read=False, write=False):
+        """
+        First checks if there are events in the socket, to avoid
+        edge trigger problems with race conditions.  Then if there
+        are none it will trampoline and when coming back check
+        for the events.
+        """
+        events = self.getsockopt(__zmq__.EVENTS)
+
+        if read and (events & __zmq__.POLLIN):
+            return events
+        elif write and (events & __zmq__.POLLOUT):
+            return events
+        else:
+            # ONLY trampoline on read events for the zmq FD
+            trampoline(self.getsockopt(__zmq__.FD), read=True)
+            return self.getsockopt(__zmq__.EVENTS)
+
 
     def _xsafe_send(self, msg, flags=0, copy=True, track=False):
         """
@@ -150,37 +186,10 @@ class Socket(__zmq__.Socket):
         the same socket.
         """
         if flags & __zmq__.NOBLOCK:
-            super(Socket, self).send(msg, flags=flags, track=track, copy=copy)
-            return
+            return super(Socket, self).send(msg, flags=flags, copy=copy, track=track)
 
-        flags |= __zmq__.NOBLOCK
-
-        if self._writers:
-            self._writers.append((msg, flags, copy, track, greenlet.getcurrent()))
-            if hubs.get_hub().switch():
-                # msg was sent by another greenthread
-                return
-            else:
-                pass
-        else:
-            self._writers.append((msg, flags, copy, track, greenlet.getcurrent()))
-
-        while True:
-            try:
-                if (self.getsockopt(__zmq__.EVENTS) & __zmq__.POLLOUT):
-                    super(Socket, self).send(msg, flags=flags, track=track,
-                                             copy=copy)
-
-                
-
-                self._sock_wait(write=True)
-                super(Socket, self).send(msg, flags=flags, track=track,
-                                         copy=copy)
-                return
-            except __zmq__.ZMQError, e:
-                if e.errno != EAGAIN:
-                    raise
-
+        self._xsafe_inner_send(msg, flags, copy, track)
+   
     def _xsafe_send_multipart(self, msg_parts, flags=0, copy=True, track=False):
         """
         A send_multipart method that's safe to use when multiple
@@ -190,52 +199,133 @@ class Socket(__zmq__.Socket):
         Ensure multipart messages are not interleaved.
         """
 
-        self._writers.append((list(reversed(msg)), flags, copy, track, greenlet.getcurrent()))
-        if len(self._writers) == 1:
-            # no blocked writers
+        if flags & __zmq__.NOBLOCK:
+            return super(Socket, self).send_multipart(msg_parts, flags=flags, copy=copy, track=track)
+
+        self._xsafe_inner_send(list(msg_parts), flags, copy, track)
+
+    def _xsafe_inner_send(self, msg, flags, copy, track):
+        is_listening = bool(self._writers or self._readers)
+
+        self._writers.append((msg, flags | __zmq__.NOBLOCK, copy, track, greenlet.getcurrent()))
+        if is_listening:
+            # Other readers or writers are blocked. If this the first writer, it may be possible to send immediately
+            if len(self._writers) == 1:
+                result = self._send_queued()
+                if not self._writers:
+                    # success!
+                    return result
             
-            pass
+            # other readers or writers are blocked so this greenthread must wait its turn
+            result = hubs.get_hub().switch()
+            if result is False:
+                # msg was not yet sent, but this thread was woken up
+                # so that it could process the queues
+                return self._process_queues()
+            else:
+                # msg was sent by another greenthread
+                return result
+        else:
+            return self._process_queues()
 
+    def _process_queues(self):
+        """ If there are readers or writers queued, this method tries
+        to recv or send messages and ensures processing continues
+        either in this greenthread or in another one. """
+        readers = self._readers
+        writers = self._writers
 
-    def _send_queued(self, ):
+        send_result = None
+        recv_result = None
+        while True:
+            events = self.getsockopt(__zmq__.EVENTS)
+            try:
+                if readers and (events & __zmq__.POLLIN):
+                    recv_result = self._recv_queued()
+
+                if writers and (events & __zmq__.POLLOUT):
+                    send_result = self._send_queued()
+            except (SystemExit, KeyboardInterrupt):
+                raise
+            except:
+                # an error occurred for this greenthread's send/recv
+                # call. Wake another thread to continue processing.
+                if readers:
+                    hubs.get_hub().schedule_call_global(0, readers[0].switch, False)
+                elif writers:
+                    hubs.get_hub().schedule_call_global(0, writers[0][-1].switch, False)
+                raise
+
+            # send and recv cannot continue right now. If there are
+            # more readers or writers queued, either trampoline or
+            # wake another greenthread.
+            current = greelnet.getcurrent()
+            if (readers and readers[0] is current) or (writers and writers[0][-1] is current):
+                # Only trampoline if this thread is the next reader or writer,
+                # and ONLY trampoline on read events for zmq FDs.
+                trampoline(self.getsockopt(__zmq__.FD), read=True)
+            else:
+                if readers:
+                    hubs.get_hub().schedule_call_global(0, readers[0].switch, False)
+                elif writers:
+                    hubs.get_hub().schedule_call_global(0, writers[0][-1].switch, False)
+                return send_result or recv_result
+                
+
+    def _send_queued(self):
         """
         Send as many msgs from the writers deque as possible. Wake up
         the greenthreads for messages that are sent.
         """
         writers = self.writers
+        current = greenlet.getcurrent()
         hub = hubs.get_hub()
+        super_send = super(Socket, self).send
+        super_send_multipart = super(Socket, self).send_multipart
+
+        result = None
 
         while writers:
             msg, flags, copy, track, writer = writers[0]
-            
-            if isinstance(msg, list):
-                is_list = True
-                m = msg[-1]
-            else:
-                is_list = False
-                m = msg
             try:
-                super(Socket, self).send(m, flags=flags, track=track,
-                                         copy=copy)
-                hub.schedule_call_global(0, writer.switch, True)
+                if isinstance(msg, list):
+                    r = super_send_multipart(msg, flags=flags, copy=copy, track=track)
+                else:
+                    r = super_send(msg, flags=flags, copy=copy, track=track)
+
+                # remember this thread's result
+                if current is writer:
+                    result = r
             except (SystemExit, KeyboardInterrupt):
                 raise
             except __zmq__.ZMQError, e:
                 if e.errno == EAGAIN:
-                    
-                    pass
+                    return result
                 else:
-                    hub.schedule_call_global(0, writer.throw, e)
+                    # message failed to send
+                    writers.popleft()
+                    if current is writer:
+                        raise
+                    else:
+                        hub.schedule_call_global(0, writer.throw, e)
+                        continue
 
-
+            # move to the next msg
+            writers.popleft()
+            # wake writer
+            if current is not writer:
+                hub.schedule_call_global(0, writer.switch, r)
+        return result    
+                    
     def _xsafe_recv(self, flags=0, copy=True, track=False):
         """
         A recv method that's safe to use when multiple greenthreads
         are calling send, send_multipart, recv and recv_multipart on
         the same socket.
         """
-        pass
 
+        if flags & __zmq__.NOBLOCK:
+            return super(Socket, self).recv(flags=flags, copy=copy, track=track)
 
     def _xsafe_recv_multipart(self, flags=0, copy=True, track=False):
         """
@@ -243,4 +333,5 @@ class Socket(__zmq__.Socket):
         are calling send, send_multipart, recv and recv_multipart on
         the same socket.
         """
-        pass
+        if flags & __zmq__.NOBLOCK:
+            return super(Socket, self).recv_multipart(flags=flags, copy=copy, track=track)
