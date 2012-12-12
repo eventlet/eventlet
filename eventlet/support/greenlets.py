b075ce1586a3ebe54ce39d93ac4799ba84e50e72
Edward George <edwardgeorge@gmail.com>
2012-12-12 22:25:22 +0000

    do not clear sys.exc_info if can be preserved.

--- eventlet/support/greenlets.py
+++ eventlet/support/greenlets.py
@@ -1,7 +1,11 @@
+import distutils.version
+
 try:
     import greenlet
     getcurrent = greenlet.greenlet.getcurrent
     GreenletExit = greenlet.greenlet.GreenletExit
+    preserves_excinfo = (distutils.version.LooseVersion(greenlet.__version__)
+            >= distutils.version.LooseVersion('0.3.2'))
     greenlet = greenlet.greenlet
 except ImportError, e:
     raise
@@ -9,14 +13,17 @@ except ImportError, e:
         from py.magic import greenlet
         getcurrent = greenlet.getcurrent
         GreenletExit = greenlet.GreenletExit
+        preserves_excinfo = False
     except ImportError:
         try:
             from stackless import greenlet
             getcurrent = greenlet.getcurrent
             GreenletExit = greenlet.GreenletExit
+            preserves_excinfo = False
         except ImportError:
             try:
                 from support.stacklesss import greenlet, getcurrent, GreenletExit
+                preserves_excinfo = False
                 (greenlet, getcurrent, GreenletExit) # silence pyflakes
             except ImportError, e:
                 raise ImportError("Unable to find an implementation of greenlet.")
