0e86812978ade01b5c79d21665195d7127439f35
Thomas Grainger <tagrain@gmail.com>
2013-01-23 20:39:02 +0000

    setup: remove --without-greenlet option

--- setup.py
+++ setup.py
@@ -5,13 +5,6 @@ from os import path
 import sys
 
 
-requirements = []
-for flag, req in [('--without-greenlet', 'greenlet >= 0.3')]:
-    if flag in sys.argv:
-        sys.argv.remove(flag)
-    else:
-        requirements.append(req)
-
 setup(
     name='eventlet',
     version=__version__,
@@ -20,7 +13,9 @@ setup(
     author_email='eventletdev@lists.secondlife.com',
     url='http://eventlet.net',
     packages=find_packages(exclude=['tests', 'benchmarks']),
-    install_requires=requirements,
+    install_requires=(
+        'greenlet >= 0.3',
+    )
     zip_safe=False,
     long_description=open(
         path.join(
