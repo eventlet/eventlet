42b585d08a7b3b53d34fe4836ba4da7162f034d4
Sergey Shepelev <temotor@gmail.com>
2013-01-24 02:09:34 +0400

    setup: greenlet is actually required, remove --without-greenlet option, remove unused httplib2 test dependency; Thanks to Thomas Grainger https://github.com/eventlet/eventlet/pull/12

--- setup.py
+++ setup.py
@@ -2,15 +2,7 @@
 from setuptools import find_packages, setup
 from eventlet import __version__
 from os import path
-import sys
 
-
-requirements = []
-for flag, req in [('--without-greenlet', 'greenlet >= 0.3')]:
-    if flag in sys.argv:
-        sys.argv.remove(flag)
-    else:
-        requirements.append(req)
 
 setup(
     name='eventlet',
@@ -20,7 +12,9 @@ setup(
     author_email='eventletdev@lists.secondlife.com',
     url='http://eventlet.net',
     packages=find_packages(exclude=['tests', 'benchmarks']),
-    install_requires=requirements,
+    install_requires=(
+        'greenlet >= 0.3',
+    ),
     zip_safe=False,
     long_description=open(
         path.join(
@@ -29,7 +23,6 @@ setup(
         )
     ).read(),
     test_suite='nose.collector',
-    tests_require='httplib2',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
