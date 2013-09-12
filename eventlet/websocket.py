49d5f857c6abc50165dfe458615729c2c553a840
Dmitry Orlov <dorlov@undev.ru>
2013-09-13 02:34:11 +0400

    [fix] Google Chrome sent to "WebSocket" lowercase

--- eventlet/websocket.py
+++ eventlet/websocket.py
@@ -40,7 +40,7 @@ class WebSocketWSGI(object):
 
     def __call__(self, environ, start_response):
         if not (environ.get('HTTP_CONNECTION') == 'Upgrade' and
-                environ.get('HTTP_UPGRADE') == 'WebSocket'):
+                environ.get('HTTP_UPGRADE').lower() == 'websocket'):
             # need to check a few more things here for true compliance
             start_response('400 Bad Request', [('Connection','close')])
             return []
