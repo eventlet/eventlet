5e392fea66953751985a0d64db5f561f4572a926
Victor Stinner <victor.stinner@gmail.com>
2014-11-20 12:25:47 +0100

    Fix monkey_patch() on Python 3
    The importlib module must use real thread locks, not eventlet.Semaphore.

---
+++ tests/patcher_test_importlib_lock.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+import sys
+
+import eventlet
+
+
+# no standard tests in this file, ignore
+__test__ = False
+
+
+def do_import():
+    import encodings.idna
+
+
+if __name__ == '__main__':
+    eventlet.monkey_patch()
+    threading = eventlet.patcher.original('threading')
+
+    sys.modules.pop('encodings.idna', None)
+
+    # call "import encodings.idna" in a new thread
+    thread = threading.Thread(target=do_import)
+    thread.start()
+
+    # call "import encodings.idna" in the main thread
+    do_import()
+
+    thread.join()
+    print('ok')
