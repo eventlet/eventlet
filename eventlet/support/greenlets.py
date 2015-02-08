96e2a30013a5a43b2c846e6cf63c021aac508217
Jakub Stasiak <jakub@stasiak.at>
2015-02-08 22:45:44 +0100

    Remove dead import code

--- eventlet/support/greenlets.py
+++ eventlet/support/greenlets.py
@@ -1,29 +1,8 @@
 import distutils.version
 
-try:
-    import greenlet
-    getcurrent = greenlet.greenlet.getcurrent
-    GreenletExit = greenlet.greenlet.GreenletExit
-    preserves_excinfo = (distutils.version.LooseVersion(greenlet.__version__)
-                         >= distutils.version.LooseVersion('0.3.2'))
-    greenlet = greenlet.greenlet
-except ImportError as e:
-    raise
-    try:
-        from py.magic import greenlet
-        getcurrent = greenlet.getcurrent
-        GreenletExit = greenlet.GreenletExit
-        preserves_excinfo = False
-    except ImportError:
-        try:
-            from stackless import greenlet
-            getcurrent = greenlet.getcurrent
-            GreenletExit = greenlet.GreenletExit
-            preserves_excinfo = False
-        except ImportError:
-            try:
-                from support.stacklesss import greenlet, getcurrent, GreenletExit
-                preserves_excinfo = False
-                (greenlet, getcurrent, GreenletExit)  # silence pyflakes
-            except ImportError as e:
-                raise ImportError("Unable to find an implementation of greenlet.")
+import greenlet
+getcurrent = greenlet.greenlet.getcurrent
+GreenletExit = greenlet.greenlet.GreenletExit
+preserves_excinfo = (distutils.version.LooseVersion(greenlet.__version__)
+                     >= distutils.version.LooseVersion('0.3.2'))
+greenlet = greenlet.greenlet
