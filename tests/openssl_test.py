e9f19c6df4a6e97a36a974338196a5a0bda079e4
Victor Stinner <vstinner@redhat.com>
2015-06-29 16:03:56 +0200

    Port eventlet.green.OpenSSL to Python 3
    * Fix import syntax for Python 3: use relative imports
    * Add unit test

---
+++ tests/openssl_test.py
@@ -0,0 +1,17 @@
+import tests
+
+
+def test_import():
+    # https://github.com/eventlet/eventlet/issues/238
+    # Ensure that it's possible to import eventlet.green.OpenSSL.
+    # Most basic test to check Python 3 compatibility.
+    try:
+        import OpenSSL
+    except ImportError:
+        raise tests.SkipTest('need pyopenssl')
+
+    import eventlet.green.OpenSSL.SSL
+    import eventlet.green.OpenSSL.crypto
+    import eventlet.green.OpenSSL.rand
+    import eventlet.green.OpenSSL.tsafe
+    import eventlet.green.OpenSSL.version
