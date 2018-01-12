3b5883c0005a51e956461877f5fbb4a1290e9587
Sergey Shepelev <temotor@gmail.com>
2018-01-12 03:01:47 +0300

    v0.22.0 release

--- eventlet/__init__.py
+++ eventlet/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-version_info = (0, 21, 0)
+version_info = (0, 22, 0)
 __version__ = '.'.join(map(str, version_info))
 # This is to make Debian packaging easier, it ignores import
 # errors of greenlet so that the packager can still at least
