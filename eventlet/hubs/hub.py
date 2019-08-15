2ecbf9cf97fb082977a5f794fd622e360deba849
Jon Schlueter <jschluet@redhat.com>
2019-08-15 10:20:01 -0400

    Only install monotonic on python2
    monotonic is a backport of time.monotonic from python3. Only install it
    for python2.

--- eventlet/hubs/hub.py
+++ eventlet/hubs/hub.py
@@ -22,7 +22,12 @@ else:
 import eventlet.hubs
 from eventlet.hubs import timer
 from eventlet.support import greenlets as greenlet, clear_sys_exc_info
-import monotonic
+try:
+    from monotonic import monotonic as now
+except ImportError:
+    import time
+    now = time.monotonic
+
 import six
 
 g_prevent_multiple_readers = True
@@ -120,7 +125,7 @@ class BaseHub(object):
         self.closed = []
 
         if clock is None:
-            clock = monotonic.monotonic
+            clock = now
         self.clock = clock
 
         self.greenlet = greenlet.greenlet(self.run)
