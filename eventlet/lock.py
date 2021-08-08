299fb4229735480e30a4c9f48c6fc885334e8290
Michael Wright <mjw@methodanalysis.com>
2021-08-08 16:42:45 +0100

    fix #697 add Lock class and make API compatible

---
+++ eventlet/lock.py
@@ -0,0 +1,24 @@
+from eventlet.semaphore import Semaphore
+
+
+class Lock(Semaphore):
+
+    """A lock.
+    This is API-compatible with :class:`threading.Lock`.
+
+    It is a context manager, and thus can be used in a with block::
+
+      lock = Lock()
+      with lock:
+        do_some_stuff()
+    """
+
+    def release(self, blocking=True):
+        """Modify behaviour vs :class:`Semaphore` to raise a RuntimeError
+        exception if the value is greater than zero. This corrects behaviour
+        to realign with :class:`threading.Lock`.
+        """
+        if self.counter > 0:
+            raise RuntimeError("release unlocked lock")
+
+        return super(Lock, self).release(blocking=blocking)
