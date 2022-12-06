cb80c0ab81373f079ae5a139adc2521519c8f95c
Sergey Shepelev <temotor@gmail.com>
2022-12-06 15:56:21 +0300

    green urlopen was blocking on https
    fixes https://github.com/eventlet/eventlet/issues/457

--- tests/green_http_test.py
+++ tests/green_http_test.py
@@ -1,6 +1,14 @@
-import eventlet
+import sys
+
 import six
+from nose.plugins.skip import SkipTest
+
+import eventlet
+import eventlet.debug
 import tests
+if sys.version_info >= (3,):
+    from eventlet.green.urllib.request import urlopen
+from tests import tool_server
 
 __test__ = six.PY3
 
@@ -22,3 +30,21 @@ def test_http_request_encode_chunked_kwarg():
     addr = server_sock.getsockname()
     h = client.HTTPConnection(host=addr[0], port=addr[1])
     h.request('GET', '/', encode_chunked=True)
+
+
+@tests.skip_if(sys.version_info < (3,))
+def test_urlopen_http_concurrent():
+    eventlet.debug.hub_blocking_detection(True)
+    with tool_server.http_server_const() as url:
+        r = urlopen(url, timeout=1)
+        assert r.status == 200
+    eventlet.debug.hub_blocking_detection(False)
+
+
+@tests.skip_if(sys.version_info < (3,))
+def test_urlopen_https_concurrent():
+    eventlet.debug.hub_blocking_detection(True)
+    with tool_server.http_server_const(tls=True) as url:
+        r = urlopen(url, timeout=1, cafile=tool_server.CA_CERTS)
+        assert r.status == 200
+    eventlet.debug.hub_blocking_detection(False)
