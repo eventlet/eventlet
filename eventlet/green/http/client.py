34029227e9636a92fe3b3a255231e7c3fc15e39e
Nat Goodspeed <nat@lindenlab.com>
2023-10-31 19:47:11 -0400

    Python 3.12 fixes by hroncok
    Credit to @hroncok and Victor Stinner <vstinner@python.org> for logic (#797).
    This PR attempts to add compatibility for older Python versions.
    
    We draw the line at Python versions that predate ssl.SSLContext, though. The
    remaining Python 2.7 documentation doesn't even mention the version at which
    that was introduced.

--- eventlet/green/http/client.py
+++ eventlet/green/http/client.py
@@ -1450,17 +1450,37 @@ try:
 except ImportError:
     pass
 else:
-    def _create_https_context(http_version):
-        # Function also used by urllib.request to be able to set the check_hostname
-        # attribute on a context object.
-        context = ssl._create_default_https_context()
-        # send ALPN extension to indicate HTTP/1.1 protocol
-        if http_version == 11:
-            context.set_alpn_protocols(['http/1.1'])
-        # enable PHA for TLS 1.3 connections if available
-        if context.post_handshake_auth is not None:
-            context.post_handshake_auth = True
-        return context
+    try:
+        ssl.wrap_socket
+    except AttributeError:
+        # Python 3.12+
+        def _create_https_context(http_version):
+            # Function also used by urllib.request to be able to set the check_hostname
+            # attribute on a context object.
+            context = ssl._create_default_https_context()
+            # send ALPN extension to indicate HTTP/1.1 protocol
+            if http_version == 11:
+                context.set_alpn_protocols(['http/1.1'])
+            # enable PHA for TLS 1.3 connections if available
+            if context.post_handshake_auth is not None:
+                context.post_handshake_auth = True
+            return context
+
+        def _populate_https_context(context, check_hostname):
+            if check_hostname is not None:
+                context.check_hostname = check_hostname
+    else:
+        # Python 3.11 or earlier
+        def _create_https_context(http_version):
+            return ssl._create_default_https_context()
+
+        def _populate_https_context(context, check_hostname):
+            will_verify = context.verify_mode != ssl.CERT_NONE
+            if check_hostname is None:
+                check_hostname = context.check_hostname
+            if check_hostname and not will_verify:
+                raise ValueError("check_hostname needs a SSL context with "
+                                 "either CERT_OPTIONAL or CERT_REQUIRED")
 
     class HTTPSConnection(HTTPConnection):
         "This class allows communication via SSL."
@@ -1479,8 +1499,7 @@ else:
             self.cert_file = cert_file
             if context is None:
                 context = _create_https_context(self._http_vsn)
-            if check_hostname is not None:
-                context.check_hostname = check_hostname
+            _populate_https_context(context, check_hostname)
             if key_file or cert_file:
                 context.load_cert_chain(cert_file, key_file)
             self._context = context
