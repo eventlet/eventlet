92da15b5ec085e6f43e46b430532913c322e1eae
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-01-10 10:23:21 -0500

    Minimal working spawn_for_coroutine().

---
+++ eventlet/asyncio.py
@@ -0,0 +1,30 @@
+"""
+Asyncio compatibility functions.
+"""
+import asyncio
+from .greenthread import spawn
+from .event import Event
+
+__all__ = ["spawn_for_coroutine"]
+
+
+def spawn_for_coroutine(coroutine):
+    """
+    Take a coroutine or some other object that can be turned into an
+    ``asyncio`` ``Future``, turn it into a ``GreenThread``.
+    """
+    # TODO error if hub is not asyncio
+    def _run():
+        # Convert whatever we're wrapping into a Future. TODO test with
+        # relevant object types.
+        future = asyncio.ensure_future(coroutine, loop=asyncio.get_running_loop())
+        # Wait until the Future has a result.
+        has_result = Event()
+        future.add_done_callback(lambda _: has_result.send(True))
+        has_result.wait()
+        # Return the result of the Future (or raise an exception if it had an
+        # exception). TODO test both cases directly.
+        return future.result()
+
+    # TODO what happens to Future if GreenThread is killed?
+    return spawn(_run)
