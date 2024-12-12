7adf378c7fe9ceef1a43af9d7814753a8117f9d8
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-12-12 14:08:15 -0500

    More testing

--- tests/isolated/asyncio_correct_patching.py
+++ tests/isolated/asyncio_correct_patching.py
@@ -2,13 +2,31 @@
 asyncio submodules continue to have the original, real socket module even after
 monkeypatching.
 """
-import eventlet
-eventlet.monkey_patch()
+
+import sys
+
+def assert_correct_patching():
+    from eventlet.greenio.base import GreenSocket
+    import asyncio.selector_events
+    if asyncio.selector_events.socket.socket is GreenSocket:
+        raise RuntimeError("Wrong socket class, should've been normal socket.socket")
+
+    import asyncio.selector_events
+    if asyncio.selector_events.selectors is not sys.modules["__original_module_selectors"]:
+        raise RuntimeError("Wrong selectors")
+
+    if asyncio.selector_events.selectors.select is not sys.modules["__original_module_select"]:
+        raise RuntimeError("Wrong select")
+
+
 import eventlet.hubs
 eventlet.hubs.get_hub()
+assert_correct_patching()
 
-from eventlet.greenio.base import GreenSocket
-import asyncio.selector_events
-if asyncio.selector_events.socket.socket is GreenSocket:
-    raise RuntimeError("Wrong socket class, should've been normal socket.socket")
+import eventlet
+eventlet.monkey_patch()
+assert_correct_patching()
+
+
 print("pass")
+
